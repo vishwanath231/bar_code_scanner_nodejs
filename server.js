68a95bf68167e89b2c822a41e04035232bec287b
Vishwanath <vishwanathvishwabai@gmail.com>
2024-04-01 22:49:43 +0530

    final

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const __dirname = path.resolve()
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
 
 
 // ejs template 
